277f1c09c5045dd468b06d97a6437e7b36636b37
Jaylo <jaylogalam04@gmail.com>
2026-01-09 16:42:35 +0800

    slight refactoring

--- src/App.tsx
+++ src/App.tsx
@@ -2,7 +2,7 @@ import { Route, Routes } from "react-router-dom";
 import "./index.css";
 
 /* Landing Page */
-import LandingPage from "@pages/LandingPage";
+import LandingPage from "@/pages/LandingPage";
 
 function App() {
   return (
