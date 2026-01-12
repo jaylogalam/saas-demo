8c625c9f5942ffd6ddc7e6552715a8e1aabd1e7f
Jaylo <jaylogalam04@gmail.com>
2026-01-13 03:58:46 +0800

    removed analyzer

--- vite.config.ts
+++ vite.config.ts
@@ -2,11 +2,10 @@ import { defineConfig } from "vite";
 import react from "@vitejs/plugin-react";
 import tailwindcss from "@tailwindcss/vite";
 import path from "path";
-import { analyzer } from "vite-bundle-analyzer";
 
 // https://vite.dev/config/
 export default defineConfig({
-  plugins: [react(), tailwindcss(), analyzer()],
+  plugins: [react(), tailwindcss()],
 
   /* Configure: Build */
   build: {
