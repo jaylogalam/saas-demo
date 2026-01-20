fd750edf77b4b27337bbc156b9d7361102808a12
Jaylo <jaylogalam04@gmail.com>
2026-01-21 01:23:21 +0800

    add shared files for edge functions

---
+++ supabase/functions/_shared/cors.ts
@@ -0,0 +1,12 @@
+export const corsHeaders = {
+  "Access-Control-Allow-Origin": "*",
+  "Access-Control-Allow-Headers": "authorization, x-client-info, apikey, content-type",
+  "Access-Control-Allow-Methods": "POST, GET, OPTIONS",
+};
+
+// A helper to handle the boilerplate OPTIONS check
+export function handleCORS(req: Request) {
+  if (req.method === "OPTIONS") {
+    return new Response("ok", { headers: corsHeaders });
+  }
+}
