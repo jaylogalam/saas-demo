19719c441429cfb4fb651a3f8e69809b4e95dffb
Jaylo <jaylogalam04@gmail.com>
2026-01-31 14:11:29 +0800

    fix subscription id name type

--- src/types/subscription.types.ts
+++ src/types/subscription.types.ts
@@ -11,7 +11,7 @@ export interface SubscriptionPlan {
 }
 
 export interface UserSubscription {
-  id: string;
+  subscriptionId: string;
   userId: string;
   email: string;
   plan: SubscriptionPlan;
